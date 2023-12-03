22f6e46e3016f278103dc784924c4f3aef2867dc
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2023-12-03 18:05:41 +0000

    complete zero division error example

--- and_then.py
+++ and_then.py
@@ -11,14 +11,16 @@ print(Err(None).and_then(inc))  # Err(None)
 x = ZeroDivisionError("5 was divided by 0")
 print(Err(x))
 
+
 def verbose_error_print(err: Err):
     match err:
         case Err(KeyError(message)):
-            print(message)  # or some fancy message
-        case Err(ZeroDivisionError(message)):
-            print(message)  # or some fancy message
+            print("There was a key error", err.value)  # or some fancy message
+        case Err(ZeroDivisionError()):
+            print("There was division error", err.value)  # or some fancy message
         case _:
             print(err)  # or some fancy message
+
 
 def print_result(r: Result):
     match r:
@@ -26,4 +28,7 @@ def print_result(r: Result):
             print("The result is ok:", content)
         case Err(content):
             print("The result was not ok!")
-            print(err)
+            verbose_error_print(r)
+
+
+print_result(Err(ZeroDivisionError("5 was divided by 0")))
